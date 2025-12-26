3376c810caaf342083d871872501b0f26ad71b9e
20200961 <kmch009@sungkyul.ac.kr>
2025-12-26 17:09:14 +0900

    first commit

---
+++ sampick_Back/src/main/java/com/example/sampick/SampickApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampickApplication.class, args);
+	}
+
+}
---
+++ sampick_Back/src/test/java/com/example/sampick/SampickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
